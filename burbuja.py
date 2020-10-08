316b82056b053083085ba4b0a5c43ba3048c8eb7
Jose Murillo <61992573+josemurillosancho@users.noreply.github.com>
2020-10-07 23:25:02 -0600

    Update burbuja.py

--- burbuja.py
+++ burbuja.py
@@ -11,4 +11,21 @@ def swap(A, i, j):
     A[j] = tmp
 
 def burbuja_optimizado(A):
-    raise NotImplementedError()
+    """Funcion para ordernar elementos de una lista
+	E: Una lista
+	S: Una lista o un error
+	R: Ocupa ser una lista"""
+	if type(A)!= list:
+		return "Error01"
+	else:
+		i = 0
+		n = len(A)
+		ordenado = False
+		while i < (n-1) and ordenado != True:
+			i += 1
+			ordenado = True
+			for j in range(0, n-i):
+				if A[j] > A[j+1]:
+					swap(A, j, j+1)
+					ordenado = False
+	return A
